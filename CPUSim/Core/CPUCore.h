1e70af7821c79a5bab7bf2af696be2df4cd6a95c
MinhCao646 <84723247+MinhCao646@users.noreply.github.com>
2026-08-21 12:18:03 +0700

    Enhance CPU constructor with screen dimensions
    Added screen height and width parameters to CPU constructor.

--- CPUSim/Core/CPUCore.h
+++ CPUSim/Core/CPUCore.h
@@ -15,13 +15,14 @@ class CPU
 private:
     int programCounter = 0;
     bool running;
+    int ScrHeight, ScrWidth;
     Render rend;
     Stack stack;
     BitMaskFlag bit;
     std::vector<int> memory;
     std::vector<Instruction> programMemory;
 public:
-    CPU(size_t memorySize = 256) : memory(memorySize), running(true) {}
+    CPU(size_t memorySize = 256, int scrHeight = 10, int scrWidth= 20) : memory(memorySize), running(true), ScrHeight(scrHeight), ScrWidth(scrWidth), rend(ScrHeight, ScrWidth) {}
 
     void LoadProgram      (const std::vector<Instruction> &program);
     Instruction Fetch     ();
